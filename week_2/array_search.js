470f00c29b0cebed9726134e8fecfdf61ad24867
adrian <98074427+adrian-guzman-301@users.noreply.github.com>
2025-11-23 08:33:42 -0800

    Create array_search.js

---
+++ week_2/array_search.js
@@ -0,0 +1,27 @@
+/*
+
+You are given an array of **Gears of War weapons** `arr`  
+and a specific weapon `target`.
+
+Your task is to implement a function `findLastIndex(arr, target)`  
+that returns the **last index** at which the target weapon appears  
+in the weapon list.
+
+If the target weapon does **not** appear in the array,  
+the function must return **-1**.
+
+Examples (Weapons Edition):
+
+console.log(findLastIndex(["Lancer", "Gnasher", "Boomshot", "Lancer", "Torque Bow"], "Lancer"));  
+// Output: 3
+
+console.log(findLastIndex(["Longshot", "Hammerburst", "Scorcher"], "Gnasher"));  
+// Output: -1
+
+console.log(findLastIndex(["Boltok", "Lancer", "Boltok", "Boomshot", "Boltok"], "Boltok"));  
+// Output: 4
+
+console.log(findLastIndex(["Retro Lancer", "Mulcher", "Breakermace"], "Gnasher"));  
+// Output: -1
+
+*/
