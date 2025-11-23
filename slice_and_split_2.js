29e16471435ffe5bd834c749db8ec484550dbab2
adrian <98074427+adrian-guzman-301@users.noreply.github.com>
2025-11-23 08:30:28 -0800

    Create slice_and_split_2.js

---
+++ slice_and_split_2.js
@@ -0,0 +1,43 @@
+/* 
+=======================================
+                SPLIT
+      (Gears of War Weapons Edition)
+======================================= 
+*/
+
+/*
+-Example 1: Splitting a Weapon Arsenal into Individual Items-
+Prompt: Split a list of weapons into separate entries using space as the separator.
+*/
+
+let weaponLoadout = "Lancer Longshot Gnasher Boomshot Mulcher";
+
+
+/*
+-Example 2: Splitting a Supply Cache (CSV Style)-
+Prompt: Split a comma-separated list of weapons into an array.
+*/
+
+let weaponCache = "Torque Bow,Scorcher,Hammerburst,Boltok";
+
+
+/*
+-Example 3: Limiting the Number of Splits-
+Prompt: Split a string of weapons into a limited number of substrings.
+*/
+
+let limitedSplitWeapons = "Boomshot:Mulcher:Breaker Mace:Retro Lancer";
+
+
+/*
+-PRACTICE: Split & Solve-
+Given the string "Lancer, Gnasher; Longshot, Torque Bow; Boomshot".
+Split the string by ';', and find the number of weapon groups.
+*/
+
+const weaponString = "Lancer, Gnasher; Longshot, Torque Bow; Boomshot";
+
+function splitFunc(weaponString) {
+    // your solution here
+}
+```
